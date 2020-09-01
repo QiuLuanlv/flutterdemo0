5dcabf46c7dee01e50c3803972026194f7ebd016
QiuLuanlv <3395487336@qq.com>
2020-09-01 17:02:53 +0800

    1

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,111 @@
-import 'package:flutter/material.dart';
 
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 import 'navigator/tab_navigator.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+//class MyApp extends StatelessWidget {
+//  @override
+//  Widget build(BuildContext context) {
+//    return MaterialApp(
+//      title: 'Flutter之旅',
+//      theme: ThemeData(
+//        primarySwatch: Colors.blue,
+//      ),
+//      home: TabNavigator(),
+//    );
+//  }
+//}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  String showResult='';
+  Future<CommonModel> fetchPost() async{
+    final response=await http.get('http://www.devio.org/io/flutter_app/json/test_common_model.json');
+    Utf8Decoder utf8decoder=Utf8Decoder();
+    final result=json.decode(utf8decoder.convert(response.bodyBytes));
+    return CommonModel.fromJson(result);
+  }
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter之旅',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('future/futurebuilder'),
+        ),
+        body: FutureBuilder<CommonModel>(
+          future: fetchPost(),
+            builder: (BuildContext context, AsyncSnapshot<CommonModel> snapshot){
+          switch(snapshot.connectionState){
+            case ConnectionState.none:
+              return Text('input a url start');
+            case ConnectionState.waiting:
+              return Center(child: CircularProgressIndicator(),);
+            case ConnectionState.active:
+              return Text('');
+            case ConnectionState.done:
+              if(snapshot.hasError){
+                return new Text(
+                  '${snapshot.error}',
+                  style: TextStyle(color: Colors.red),
+                );
+              }else{
+                return new Column(
+                  children: <Widget>[
+                    Text('icon:${snapshot.data.icon}'),
+                    Text('statusBarColor:${snapshot.data.statusBarColor}'),
+                    Text('title:${snapshot.data.title}'),
+                    Text('url:${snapshot.data.url}'),
+                  ],
+                );
+              }
+          }
+        }),
+//        body: Column(
+//          children: <Widget>[
+//            InkWell(
+//              onTap: (){
+//                fetchPost().then((CommonModel value){
+//                  setState(() {
+//                    showResult='返回：\nstatusBarColor:${value.statusBarColor},\nurl:${value.url}';
+//                  });
+//                });
+//              },
+//              child: Text(
+//                'click me',
+//                style: TextStyle(fontSize: 26),
+//              ),
+//            ),
+//            Text(showResult)
+//          ],
+//        ),
       ),
-      home: TabNavigator(),
     );
   }
+}
+
+class CommonModel{
+  final String icon;
+  final String title;
+  final String url;
+  final String statusBarColor;
+  final bool hideAppBar;
+
+  CommonModel({this.icon, this.title, this.url, this.statusBarColor, this.hideAppBar});
+  factory CommonModel.fromJson(Map<String, dynamic> json){
+    return CommonModel(
+      icon: json['icon'],
+      title: json['title'],
+      url: json['url'],
+      statusBarColor: json['statusBarColor'],
+      hideAppBar: json['hideAppBar'],
+    );
+  }
+
 }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_swiper/flutter_swiper.dart';
+const APPBAR_SCROLL_OFFSET=100;
 class HomePage extends StatefulWidget{
   @override
   _HomePageState createState() => _HomePageState();
@@ -8,34 +9,82 @@ class HomePage extends StatefulWidget{
 
 class _HomePageState extends State<HomePage>{
   List _imageUrls=[
-    'https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=2127793287,3926661110&fm=26&gp=0.jpg',
-    'https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=2127793287,3926661110&fm=26&gp=0.jpg',
+    'https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=1898565886,53348875&fm=26&gp=0.jpg',
+    'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3442736072,448372868&fm=26&gp=0.jpg',
     'https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=2127793287,3926661110&fm=26&gp=0.jpg',
   ];
+  double appBarAplha=0;
+  _onScroll(offset){
+    double alpha=offset/APPBAR_SCROLL_OFFSET;
+    if(alpha<0){
+      alpha=0;
+    }else if(alpha>1){
+      alpha=1;
+    }
+    setState(() {
+      appBarAplha=alpha;
+    });
+
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          children: <Widget>[
-            Container(
-              height: 160,
-              child: ListView(
-                children: <Widget>[
-                  Swiper(
-                    itemCount: _imageUrls.length,
-                    autoplay: true,
-                    itemBuilder: (BuildContext context,int index){
-                      return Image.network(_imageUrls[index],fit: BoxFit.fill,);
-                    },
-                    pagination: SwiperPagination(),
-                  ),
-                ],
+      body:Stack(
+        children: <Widget>[
+          MediaQuery.removePadding(
+              removeTop: true,
+              context: context,
+              child: NotificationListener(
+                onNotification: (scrollNotification){
+                  if(scrollNotification is ScrollUpdateNotification &&
+                      scrollNotification.depth==0){
+                    //滚动且是列表滚动的时候
+                    _onScroll(scrollNotification.metrics.pixels);
+                  }
+                },
+                child: ListView(
+                  children: <Widget>[
+                    Container(
+                      height: 160,
+                      child: ListView(
+                        children: <Widget>[
+                          Swiper(
+                            itemCount: _imageUrls.length,
+                            autoplay: true,
+                            itemBuilder: (BuildContext context,int index){
+                              return Image.network(_imageUrls[index],fit: BoxFit.fill,);
+                            },
+                            pagination: SwiperPagination(),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Container(
+                      height: 800,
+                      child: ListTile(
+                        title: Text('hahahahah???'),
+                      ),
+                    ),
+                  ],
+                ),
+              )
+          ),
+          Opacity(
+            opacity: appBarAplha,
+            child: Container(
+              height: 80,
+              decoration: BoxDecoration(color: Colors.black12),
+              child: Center(
+                child: Padding(
+                  padding: EdgeInsets.only(top:20),
+                  child: Text('首页'),
+                ),
               ),
-            )
-          ],
-        ),
-      ),
+            ),
+          ),
+        ],
+      )
+
     );
   }
   
